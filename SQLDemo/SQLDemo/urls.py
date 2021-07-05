f2576968f12568c14f23f0d6bec768327552ef58
kparmar77 <kashyap.parmar@insuradar.com>
2021-07-05 11:36:12 +0530

    First Commit

---
+++ SQLDemo/SQLDemo/urls.py
@@ -0,0 +1,26 @@
+"""SQLDemo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from selectApp.views import  FormViewData, List
+
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', FormViewData, name='select'),
+    path('list/', List, name='selectList'),
+]
